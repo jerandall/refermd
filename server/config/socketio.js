f7b0b459ac3f3ecd253ec0cc351b16836bcc3c73
Jason Randall <jerandall@contentfusion.net>
2017-05-03 14:07:06 -0500

    update

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -17,7 +17,7 @@ function onConnect(socket) {
 
 	// Insert sockets below
 require('../api/patient/patient.socket').register(socket);
-require('../api/practice/practice.socket').register(socket);
+
 require('../api/shifts/shifts.socket').register(socket);
 	require('../api/appointment/appointment.socket').register(socket);
 
