1873cf047814c0152a9df66f0b29f30b88c1c05a
Jason Randall <jerandall@contentfusion.net>
2017-05-09 18:03:01 -0500

    update

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -4,11 +4,11 @@
 
 	class DashboardController {
 
-		constructor($http, $scope, $rootScope, $state, $compile, $timeout, socket, Auth, ReferralService, User, Shifts) {
+		constructor($http, $scope, $rootScope, $state, $compile, $timeout, socket, Auth, AppointmentService, User, Shifts) {
 			var vm = this;
 			this.$http = $http;
 			this.awesomeThings = [];
-			this.referrals = [];
+			this.appointments = [];
 			this.slots = [];
 			this.isLoggedIn = Auth.isLoggedIn;
 			this.isAdmin = Auth.isAdmin;
