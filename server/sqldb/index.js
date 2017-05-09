14c5dbd6b8938be5bb099aeba06081cf8eb1ab46
Jason Randall <jerandall@contentfusion.net>
2017-05-09 12:11:10 -0500

    update

--- server/sqldb/index.js
+++ server/sqldb/index.js
@@ -22,6 +22,8 @@ db.Practice = db.sequelize.import('../api/practice/practice.model');
 
 db.Shifts = db.sequelize.import('../api/shifts/shifts.model');
 db.Appointment = db.sequelize.import('../api/appointment/appointment.model');
+db.Referral = db.sequelize.import('../api/referral/referral.model');
+
 db.User = db.sequelize.import('../api/user/user.model');
 
 
@@ -71,5 +73,35 @@ db.Appointment.belongsTo(db.User, {
 });
 // Relationship ends
 
+db.User.hasMany(db.Referral, {
+	foreignKey: 'UserId'
+});
+db.Referral.belongsTo(db.User, {
+	as: 'creator',
+	foreignKey: 'UserId',
+	targetKey: '_id',
+	constraints: false
+});
+
+
+db.User.hasMany(db.Referral, {
+	foreignKey: 'PatientId'
+});
+db.Referral.belongsTo(db.User, {
+	as: 'Patient',
+	foreignKey: 'PatientId',
+	targetKey: '_id',
+	constraints: false
+});
+
+db.User.hasMany(db.Referral, {
+	foreignKey: 'PhysicianId'
+});
+db.Referral.belongsTo(db.User, {
+	as: 'Physician',
+	foreignKey: 'PhysicianId',
+	targetKey: '_id',
+	constraints: false
+});
 
 export default db;
